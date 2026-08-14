3eec30d03ca610e69a10e21ca120e4c726dc4228
Jessi <victorjsnogueira23@gmail.com>
2026-08-14 18:56:34 -0300

    Update selinuxfs.c

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -164,7 +164,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 #ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	// If always enforce option is set, selinux is always enforcing
 	new_value = 1;
-elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
+#elif defined(CONFIG_SECURITY_SELINUX_ALWAYS_PERMISSIVE)
 	// If always permissive option is set, selinux is always permissive
 	new_value = 0;
 #endif
